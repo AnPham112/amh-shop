f0432128d831dc67927602ad4ddf72e5eb654d6c
anpham112 <anpham1122000@gmail.com>
2022-04-10 10:35:20 +0700

    add payment

--- controllers/paymentCtrl.js
+++ controllers/paymentCtrl.js
@@ -1,6 +1,6 @@
-const Payments = require('../models/paymentModal')
 const Users = require('../models/userModel');
 const Products = require('../models/productModel');
+const Payments = require('../models/paymentModel')
 
 const paymentCtrl = {
   getPayments: async (req, res) => {
@@ -16,10 +16,31 @@ const paymentCtrl = {
     try {
       const user = await Users.findById(req.user.id).select('name email')
       if (!user) return res.status(400).json({ msg: "User does not exist" })
+
+      const { cart, paymentID, address } = req.body;
+      const { _id, name, email } = user;
+
+      const newPayment = new Payments({
+        user_id: _id, name, email, cart, paymentID, address
+      })
+
+      cart.filter(item => {
+        return sold(item._id, item.quantity, item.sold)
+      })
+
+      await newPayment.save()
+
+      res.json({ msg: "Payment success!" })
     } catch (err) {
       return res.status(500).json({ msg: err.message })
     }
   }
+}
+
+const sold = async (id, quantity, oldSold) => {
+  await Products.findOneAndUpdate({ _id: id }, {
+    sold: quantity + oldSold
+  })
 }
 
 module.exports = paymentCtrl
